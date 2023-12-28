85cb0584a8823067a7d6dec750f3dd97b5c7cfc9
jeffwinograd <jwinograd1@gmail.com>
2023-12-27 19:13:09 -0600

    Added SampleStore with hard-coded customer list. Added SampleService with axios. Reworked TheWelcome component to display SampleStore state.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,11 @@
 <template>
   <div>Welcome</div>
   <h4 v-for="(name, index) in customers" :key="index">{{ name }}</h4>
+
+  <h5>
+    The first Customer is:
+    <span style="font-style: italic">{{ sampleStore.getFirstCustomer }}</span>
+  </h5>
 </template>
 
 <script setup lang="ts">
