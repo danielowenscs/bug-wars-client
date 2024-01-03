41cdf4a3489f035afc0f306f87fab400a80aa16b
Icerice965 <icerice965@gmail.com>
2024-01-02 23:49:09 -0500

    Homepage completed

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -1,12 +1,26 @@
-import { describe, expect, it } from 'vitest';
+// import { describe, expect, it } from 'vitest';
 
-import TheWelcome from '@/components/TheWelcome.vue';
-import HomeView from '@/views/HomeView.vue';
+// import TheWelcome from '@/components/TheWelcome.vue';
+// import HomeView from '@/views/HomeView.vue';
+// import { shallowMount } from '@vue/test-utils';
+
+// describe('HomeView.vue', () => {
+//   it('renders TheWelcome component', () => {
+//     const wrapper = shallowMount(HomeView);
+//     expect(wrapper.findComponent(TheWelcome).exists()).toBe(true);
+//   });
+// });
+
 import { shallowMount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+import HomeView from '@/views/HomeView.vue';
 
-describe('HomeView.vue', () => {
-  it('renders TheWelcome component', () => {
+
+describe('HomeView', () => {
+  it('renders properly', () => {
     const wrapper = shallowMount(HomeView);
-    expect(wrapper.findComponent(TheWelcome).exists()).toBe(true);
+    expect(wrapper.findComponent(HomeView).exists()).toBe(true);
   });
 });
+
+
