009afba636a2d0a13f5ff2b83daa8dc8bac72a00
Daniel Owens <danielowenscs@gmail.com>
2024-01-05 20:26:22 -0500

    created delete script

---
+++ src/components/DeleteScript.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Are you sure you want to delete this script</h1>
+        <button @click="handleYes">Yes</button>
+        <button @click="handleNo">No</button>
+    </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, defineEmits } from 'vue';
+import { useScriptStore } from '@/stores/ScriptStore';
+
+const emits = defineEmits(['noClicked']);
+
+
+const handleYes = () => {
+    console.log('we deleted', script);
+}
+
+const handleNo = () => {
+    emits('noClicked');
+}
+
+const scriptStore = useScriptStore();
+let script;
+
+onMounted(() => {
+  script = scriptStore.script;
+  console.log(script.scriptId);
+});
+</script>
--- src/views/ScriptDetailView.vue
+++ src/views/ScriptDetailView.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
     <ScriptDetail />
+    <button @click="handleDelete">Delete</button>
+    <div v-if="showDelete">
+      <DeleteScript @noClicked="handleNo"/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import DeleteScript from '@/components/DeleteScript.vue';
 import ScriptDetail from '@/components/ScriptDetail.vue';
+import { ref } from 'vue';
+
+
+const handleNo = () => {
+  showDelete.value = false;
+}
+
+let showDelete = ref(false);
+
+const handleDelete = () => {
+    console.log('handle delete');
+    showDelete.value = true;
+  }
+
 </script>
 
-<style scoped></style>
+
