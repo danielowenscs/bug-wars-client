55b71a9e99ccb65d9ac65641d45b7b07eafbe285
jeffwinograd <jwinograd1@gmail.com>
2024-01-16 09:59:09 -0600

    added toast message to DeleteScript

--- src/components/DeleteScript.vue
+++ src/components/DeleteScript.vue
@@ -13,18 +13,19 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';
 import { useScriptStore } from '@/stores/ScriptStore';
 import scriptService from '../services/scriptService';
 
 const scriptStore = useScriptStore();
 let showDelete = ref(false);
+const toast = useToast();
 
 const script = computed(() => {
   return scriptStore.script;
 });
 
 const router = useRouter();
-<<<<<<< HEAD
 const toggleDelete = () => {
   console.log('handle delete');
   showDelete.value = true;
@@ -40,27 +41,6 @@ const handleDelete = async () => {
   scriptService.deleteScriptById(scriptId);
   await scriptStore.deleteScript(scriptId);
   router.push({ name: 'scripts' });
+  toast.success("Script Deleted");
 };
-=======
-let script: any;
-
-const handleDelete = () => {
-  const scriptId: number = script.scriptId;
-  scriptService.deleteScriptById(scriptId).then((response) => {
-    console.log(response);
-  });
-  router.push({ name: 'scripts' });
-};
-
-const cancelDelete = () => {
-  emits('cancelDelete');
-};
-
-const scriptStore = useScriptStore();
-
-onMounted(() => {
-  script = scriptStore.script;
-  console.log(script.scriptId);
-});
->>>>>>> 9396666 (changed script property  names to camelcase. started moving files in separate test folders.)
 </script>
