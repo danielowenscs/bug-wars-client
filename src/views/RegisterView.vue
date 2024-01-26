8671961be4605a467249aded4b6ae61e4c13c97f
jeffwinograd <jwinograd1@gmail.com>
2024-01-26 17:24:14 -0600

    add lang='ts' to script setup

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -2,53 +2,45 @@
   <div class="register-page">
     <h2>Create an Account</h2>
     <form @submit.prevent="register" class="register-form">
-      <label for="username"
-      >Username: 
-      </label>
+      <label for="username">Username: </label>
       <input
-          type="text"
-          id="username"
-          minlength="3"
-          maxlength="20"
-          v-model="newUser.username"
-          required
-        />
-      <label for="email"
-        >Email Address:
-      </label>
+        type="text"
+        id="username"
+        minlength="3"
+        maxlength="20"
+        v-model="newUser.username"
+        required
+      />
+      <label for="email">Email Address: </label>
       <input type="email" id="email" maxlength="50" v-model="newUser.email" required />
-      <label for="password"
-        >Password:
-      </label>
+      <label for="password">Password: </label>
       <input
-          type="password"
-          id="password"
-          minlength="6"
-          maxlength="40"
-          v-model="newUser.password"
-          required
-        />
+        type="password"
+        id="password"
+        minlength="6"
+        maxlength="40"
+        v-model="newUser.password"
+        required
+      />
 
-      <label for="passwordDuplicate"
-        >Re-enter Password:
-      </label>
+      <label for="passwordDuplicate">Re-enter Password: </label>
       <input
-          type="password"
-          id="passwordDuplicate"
-          minlength="6"
-          maxlength="40"
-          v-model="passwordDuplicate"
-          required
-        />
-        <span v-if="!passwordMatch">Passwords must be matching</span>
+        type="password"
+        id="passwordDuplicate"
+        minlength="6"
+        maxlength="40"
+        v-model="passwordDuplicate"
+        required
+      />
+      <span v-if="!passwordMatch">Passwords must be matching</span>
       <button type="submit">Register</button>
     </form>
-    
+
     <a href="/bug-wars-client/login" class="login-link">Already have an account? Login here</a>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import { ref, reactive } from 'vue';
 import authService from '@/services/authService';
 import { useRouter } from 'vue-router';
@@ -91,9 +83,9 @@ const register = () => {
 <style scoped>
 .register-page {
   display: grid;
-  grid-template-areas: 
-  "register-form" 
-  "login-link";
+  grid-template-areas:
+    'register-form'
+    'login-link';
   justify-content: center;
   margin: auto;
   width: 100%;
@@ -101,7 +93,7 @@ const register = () => {
 }
 
 .register-form {
-  grid-area: "register-form";
+  grid-area: 'register-form';
   display: inline-grid;
   padding-top: 5px;
 }
@@ -123,8 +115,7 @@ button {
 }
 
 .login-link {
-  grid-area: "login-link";
+  grid-area: 'login-link';
   font-size: 15px;
 }
-
 </style>
