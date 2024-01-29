1cb851902856274bce2b00a112935a12b5835e79
Maconnell <maconnell.balderas@gmail.com>
2024-01-29 14:29:53 -0600

    Updated About us page to include gmail, github, linkedin

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,20 +5,39 @@
     <div>
       <img class="image" :src="useImage()" />
     </div>
-    <p>{{ memberDescription[currentMember] }}</p>
-    <button @click="nextMember">Next Member</button>
+    <p class="description">{{ memberDescription[currentMember] }}</p>
+    <div class="icons">
+      <a @click="copyURL">
+        <img class="icon" src="../gmailLogo.png" />
+      </a>
+      <a :href="memberGithub[currentMember]">
+        <img class="icon" src="../githubLogo.png" />
+      </a>
+      <a :href="memberLinkedIn[currentMember]">
+        <img class="icon" src="../linkedInLogo.png" />
+      </a>
+    </div>
+    <button class="prev" @click="prevMember">Previous</button>
+    <button class="next" @click="nextMember">Next</button>
   </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
+import { ref } from 'vue';
 
 let currentMember = ref(0);
-let memberNames = ["Joe Dirt", "John Connor", "Ellen Ripley"];
-let memberImages = ["joeDirt.jpg", "johnConner.jpg", "ellenRipley.jpg"]
-let memberDescription = ["Howdy folks, name's Joe Dirt. Born and raised in the heart of America, I've seen my fair share of ups and downs. Life ain't always been easy for this guy with the mullet, but I've learned to roll with the punches and keep on truckin'.Growing up, I didn't have much. My family wasn't exactly the cream of the crop, and we bounced around from trailer park to trailer park more times than I can count. But you know what they say, tough times breed tough folks, and I reckon I'm as tough as they come.I've had more odd jobs than you can shake a stick at. From janitorial work to sewage maintenance, I've done it all. But no matter where I found myself, I always had a smile on my face and a wisecrack ready to go.Now, I ain't no scholar, but I've got street smarts like you wouldn't believe. I've navigated my way through some sticky situations, let me tell ya. And along the way, I've met some characters that make my life look downright normal.Sure, some folks might laugh at my style or my accent, but I ain't never let that get me down. I'm proud of who I am and where I come from, and ain't nobody gonna change that.", "Hey, I'm John Connor. Yeah, that John Connor – the guy at the center of the whole Terminator mess. My life's been one wild ride, let me tell you.Growing up, I never knew normal. My mom, Sarah Connor, she was something else. She told me stories about how I'd lead the resistance against the machines in the future. Pretty heavy stuff for a kid, huh? But hey, she wasn't wrong.When I was a teenager, everything changed. Terminators started coming back from the future, trying to off me before I could do my thing. Talk about pressure! But I guess that's the burden of being the savior of humanity, right?I've been through it all – running, fighting, you name it. But I've also had some amazing people by my side. Kyle Reese, my mom, even a reprogrammed Terminator named Uncle Bob. Yeah, you heard that right – a Terminator, on our side.It's been a rough road, though. Losing my mom was one of the hardest things I've ever had to face. But it only made me more determined to stop Judgment Day from happening.", "Hey there, I'm Ellen Ripley. If you've heard my name, chances are you know about the horrors I've faced out there in the deep, dark reaches of space. But let me start from the beginning.I was just your average space trucker, working for the Weyland-Yutani Corporation aboard the Nostromo. Our routine job turned into a nightmare when we stumbled upon a distress signal on LV-426. That's where we found the Xenomorph – a creature beyond nightmares, vicious and relentless.I did everything I could to survive that ordeal. Lost my crew, my ship, everything. But somehow, I made it out alive. That experience changed me forever.After drifting in space for decades, they found me. The Company, always looking to exploit the Xenomorph for profit. They didn't care about what I'd been through, only what they could gain. But I wasn't about to let them weaponize that horror.I faced the Xenomorph again and again, on LV-426, on Fury 161, even on Earth. Each time, I fought with everything I had, determined not to let those monsters win."]
+let memberNames = ["Daniel Owens", "Ethan Reynolds", "Maichelle Reynolds", "Jeff Winograd", "Parker Ignagni", "Andrew Balderas", "Maconnell Balderas"];
+let memberImages = ["joeDirt.jpg", "johnConner.jpg", "ellenRipley.jpg", "johnConner.jpg", "joeDirt.jpg", "Andrew.png", "johnConner.jpg"];
+let memberDescription = ["Description", "Description", "Description", "Description", "Description", "Description", "Experienced Senior Full Stack Developer with a fervent dedication to crafting enterprise and web applications within the gaming and 5G sectors, employing Agile methodologies. Possesses robust analytical prowess, adept troubleshooting abilities, and a keen eye for debugging, all underscored by a steadfast commitment to code excellence and optimal application performance. Proficient in data structures, design patterns, microservices, continuous delivery, and continuous integration, adept at swiftly grasping and implementing new technologies within dynamic, multicultural settings."];
+let memberGmail = ["danielowenscs@gmail.com", "ebud6805@gmail.com", "maichellereynolds2@gmail.com", "jwinograd1@gmail.com", "paulpignagni@gmail.com", "balderas.3@osu.edu", "maconnell.balderas@gmail.com"];
+let memberGithub = ["https://github.com/danielowenscs", "https://github.com/ethanrey19", "https://github.com/MaichelleR", "https://github.com/jeffelwino", "https://github.com/Paul-Ignagni", "https://github.com/balderax", "https://github.com/MaconnellBalderas"];
+let memberLinkedIn = ["https://www.linkedin.com/in/danielowenscs/", "https://www.linkedin.com/in/ETHAN", "https://www.linkedin.com/in/MAICHELLE", "https://www.linkedin.com/in/jeff-l-winograd/", "https://www.linkedin.com/in/paulignagni/", "https://www.linkedin.com/in/andrewbalderas", "https://www.linkedin.com/in/maconnell-balderas/"];
 
 const useImage = () => {
   return new URL(`../${memberImages[currentMember.value]}`, import.meta.url).href;
+}
+
+const copyURL = () => {
+  navigator.clipboard.writeText(memberGmail[currentMember.value])
 }
 
 const nextMember = () => {
@@ -28,9 +47,29 @@ const nextMember = () => {
     currentMember.value += 1;
   }
 };
+
+const prevMember = () => {
+  if(currentMember.value === 0) {
+    currentMember.value = memberNames.length - 1
+  } else {
+    currentMember.value -= 1;
+  }
+}
 </script>
 
 <style scoped>
+.next {
+  bottom: 10px;
+  right: 10px;
+  width: 120px;
+  position: fixed;
+}
+.prev {
+  bottom: 10px;
+  left: 10px;
+  width: 120px;
+  position: fixed;
+}
 .counters {
   display: flex;
   gap: 20px;
@@ -44,7 +83,7 @@ const nextMember = () => {
   flex-direction: column;
 
   justify-content: center;
-  gap: 20px;
+  gap: 10px;
 }
 .header {
   font-size: 40px;
@@ -53,5 +92,11 @@ img {
   border-radius: 50%;
   height: 50%;
   width: 50%;
+}
+.icon {
+  height: 50px;
+  width: 50px;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 </style>
