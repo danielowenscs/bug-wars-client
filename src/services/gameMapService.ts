b5d1489aa6b2ee79d9ad984462beac26609b9a0f
jeffwinograd <jwinograd1@gmail.com>
2024-01-29 19:37:43 -0600

    Added getAllMaps api call to mapService class. Refactored GameMapStore init() action. Created GameMap type, refactoring store in use those instead of 'any'. Added onMounted() hook to MapCarousel component.

--- src/services/gameMapService.ts
+++ src/services/gameMapService.ts
@@ -1,3 +1,15 @@
 import axios from "axios";
 
+export default{
+
+    async getAllMaps(){
+        return axios.get('/api/maps')
+        .catch(error => {
+            console.error(error);
+            return Promise.reject(error);
+        })},
+
+
+}
+
 
