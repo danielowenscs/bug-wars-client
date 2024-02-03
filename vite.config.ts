74fe5e32307d34b80400f588fdc106e3d09b58a2
Daniel Owens <danielowenscs@gmail.com>
2024-02-03 03:06:54 -0500

    changing base url to load assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bug-wars-client',
+  base: '/bug-wars-client/',
   plugins: [
     vue({
       script: {
