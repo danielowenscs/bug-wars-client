9999eb69ec0e908ebf66a043f65138e0abd1f292
Daniel Owens <danielowenscs@gmail.com>
2024-02-06 17:20:36 -0500

    firebase debugging

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bug-wars-client',
+  base: './',
   plugins: [
     vue({
       script: {
