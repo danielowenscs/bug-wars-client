5d7bc01c207e07b42db8a354cb0d6f9b493a7d35
Maconnell <maconnell.balderas@gmail.com>
2024-02-13 19:54:31 -0600

    Fixed AXIOS Condition

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ app.use(Toast);
 app.use(router);
 // TODO: Setup axios on backend ~Daniel
 //baseURL for deployment. Uncomment before committing.
-axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API || "https://bug-wars-server-fhs0.onrender.com"
+axios.defaults.baseURL = import.meta.env.PROD == false ? import.meta.env.VITE_REMOTE_API : "https://bug-wars-server-fhs0.onrender.com"
 
 app.mount('#app');
